c05b8c47d4a49387643bb505fbc2b5f6b46f6278
Rohesa123 <rohesasidiqpermana05@gmail.com>
2024-11-01 09:18:28 +0700

    [UPDATE] - Update Help command

--- commands/general/help.ts
+++ commands/general/help.ts
@@ -1,5 +1,15 @@
 import { WASocket, proto } from '@whiskeysockets/baileys';
 
 export const execute = async (sock: WASocket, message: proto.IWebMessageInfo, _: string[]) => {
-  return await sock.sendMessage(message.key.remoteJid as string, { text: 'Bacot Kont' }, { quoted: message } );
+  const helpText = `
+    Berikut adalah daftar perintah yang tersedia:
+
+    - *!dlmanga*: Mengunduh manga dalam bahasa Indonesia dari situs Westmanga dalam format PDF.
+    - *!decode*: Mengonversi teks menjadi base64 dengan format UTF-8.
+    - *!encode*: Mengonversi base64 UTF-8 menjadi teks biasa.
+    - *!sticker*: Mengubah gambar menjadi stiker.
+
+    Gunakan perintah sesuai kebutuhan Anda. Terima kasih.
+`;
+  return await sock.sendMessage(message.key.remoteJid as string, { text: helpText }, { quoted: message } );
 };
